68ddb532bef9e77848cff2c52b3f151c0b0d0b13
ntprojects <ntprojects@github.com>
2018-12-11 10:54:32 -0600

    Cleaned driver file

--- driver.py
+++ driver.py
@@ -12,21 +12,37 @@ from posTagger import wordPosTagger
 from dependencyParser import dependencyParse
 from relationExtractor import extractRelations
 
-filePointer=codecs.open("a.txt",'r',encoding='utf-8')
+print("Reading corpus")
+filePointer=codecs.open("corpus.txt",'r',encoding='utf-8',errors="ignore")
 text=filePointer.read()
 
-print(text)
-tokenizedWords=wordTokenizer(text)
-print("Tokenized Words: \n",tokenizedWords)
-tokenziedSentence=sentenceTokenizer(text)
-print("\n\nTokenized Sentence: \n",tokenziedSentence)
+def extractNLPFeatures():
+	#Tokenize the corpus into words
+	input("Press Enter to tokenize the corpus into words...")
+	tokenizedWords=wordTokenizer(text)
+	print("Tokenized Words: \n",tokenizedWords)
 
-lemmatizedWords=wordLemmatizer(tokenizedWords)
-print("\n\nLemmatized Words: \n",lemmatizedWords)
+	input("Press Enter to tokenize the corpus into sentences...")
+	tokenziedSentence=sentenceTokenizer(text)
+	print("\n\nTokenized Sentence: \n",tokenziedSentence)
 
-posTaggedwords = wordPosTagger(text)
-print("\n\nPOS Tagged Words: \n",posTaggedwords)
+	#Lemmatize the words to extract lemma as features
+	input("Press Enter to extract lemma as features...")
+	lemmatizedWords=wordLemmatizer(tokenizedWords)
+	print("\n\nLemmatized Words: \n",lemmatizedWords)
 
-#dependencyParse(text)
+	input("Press Enter to extract POS tag features...")
+	#POS tag the words to extract POS tag features
+	posTaggedwords = wordPosTagger(text)
+	print("\n\nPOS Tagged Words: \n",posTaggedwords)
 
-#extractRelations(tokenizedWords)
+	input("Press Enter to extract hypernymns, hyponyms, meronyms, AND holonyms as features ...")
+	#Using WordNet, extracting hypernymns, hyponyms, meronyms, AND holonyms as features 
+	extractRelations(tokenizedWords)
+
+	input("Press Enter to perform dependency parsing...")
+	#Perform dependency parsing or full-syntactic parsing to parse-tree based patterns as features 
+	dependencyParse(text)
+
+if __name__ == '__main__':
+	extractNLPFeatures()
